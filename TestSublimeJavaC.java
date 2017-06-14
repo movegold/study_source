d21b5a74fadfdde718780a25dedce253ba62f0f4
movegold <liuzonglinasd@163.com>
2017-06-15 01:21:43 +0800

    create new things

--- TestSublimeJavaC.java
+++ TestSublimeJavaC.java
@@ -1,4 +1,13 @@
 public class TestSublimeJavaC {
+
+	public TestSublimeJavaC(){
+		this.name = "castle";
+		this.age = 27;
+	}
+
+	private String name;
+
+	private Integer age;
 	
 	public static void main(String[] args){
 		for (int i =0; i < 1100 ;i++ ) {
@@ -6,4 +15,22 @@ public class TestSublimeJavaC {
 		}
 		
 	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public Integer getAge(){
+		return age;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public  void setAge(Integer age){
+		this.age = age;
+	}
+
 }
+
